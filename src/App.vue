e6c883edad6040d2fafc65d808df46ae572cf3f5
peruibeloko <cmendonca32@gmail.com>
2024-04-30 21:56:19 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <textarea v-model="input"></textarea>
+  <div class="vbar"></div>
+  <section class="timeline">
+    <div v-for="el of output" class="entry">
+      <div class="line"></div>
+      <div class="data">
+        <span class="date">{{ el.date }}</span>
+        <span class="caption">{{ el.caption }}</span>
+      </div>
+      <div class="line"></div>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+
+const parseLine = (input: string) => {
+  const [date, caption] = input.split(/: ?/);
+  return { date, caption };
+};
+
+const input = ref('');
+const output = computed(() => input.value.split('\n').map(parseLine));
+</script>
